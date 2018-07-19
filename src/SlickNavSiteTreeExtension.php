81b7216fab8792feb81379ce80c111233e88f09e
Zazama <jan.metzger@gmx.net>
2018-07-19 15:49:15 +0200

    fix everything

--- src/SlickNavSiteTreeExtension.php
+++ src/SlickNavSiteTreeExtension.php
@@ -2,14 +2,20 @@
 
 namespace Zazama;
 
+use SilverStripe\View\Requirements;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Extension;
+
 class SlickNavSiteTreeExtension extends Extension {
+
+	use Configurable;
 
 	private static $include_jquery = true;
 
 	public function contentcontrollerInit() {
 		Requirements::css('zazama/silverstripe-slicknav:css/slicknav.min.css');
-		if(this->config()->get('include_jquery')) {
-			Requirements::javascript('zazama/silverstripe-slicknav:javascript/jquery.min.js');
+		if($this->config()->get('include_jquery')) {
+			Requirements::javascript('zazama/silverstripe-slicknav:javascript/jquery-3.3.1.min.js');
 		}
 		Requirements::javascript('zazama/silverstripe-slicknav:javascript/jquery.slicknav.min.js');
 	}
